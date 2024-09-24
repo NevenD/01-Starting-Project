0bb3bac6e3f8045762c0b71f1a13966670b023d9
Neven Dimač <neven.dimac@gmail.com>
2024-09-24 22:11:37 +0200

    Clean js file

--- .github/actions-neven/deploy-s3-javascript/main-neven-action.js
+++ .github/actions-neven/deploy-s3-javascript/main-neven-action.js
@@ -8,7 +8,6 @@ function runNeven() {
   const distFolder = core.getInput('dist-folder', { trimWhitespace: true, required: true });
 
   const s3Uri = `s3://${bucket}`;
-  AWS_ACCESS_KEY_ID = AKIA47CRWPDPW7INTIGB;
   exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`); // aws cli send commands to a s3 bucket
 
   core.notice('Hello from my custom Javascript Action!');
