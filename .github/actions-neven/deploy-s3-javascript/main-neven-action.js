6dbd94012fbf0b1287aaa2eaa835e4d3b57e1973
Neven Dimač <neven.dimac@gmail.com>
2024-09-24 22:07:44 +0200

    Added custom deploy action

--- .github/actions-neven/deploy-s3-javascript/main-neven-action.js
+++ .github/actions-neven/deploy-s3-javascript/main-neven-action.js
@@ -3,6 +3,14 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function runNeven() {
+  const bucket = core.getInput('bucket', { trimWhitespace: true, required: true });
+  const bucketRegion = core.getInput('bucket-region', { trimWhitespace: true, required: true });
+  const distFolder = core.getInput('dist-folder', { trimWhitespace: true, required: true });
+
+  const s3Uri = `s3://${bucket}`;
+  AWS_ACCESS_KEY_ID = AKIA47CRWPDPW7INTIGB;
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`); // aws cli send commands to a s3 bucket
+
   core.notice('Hello from my custom Javascript Action!');
 }
 
