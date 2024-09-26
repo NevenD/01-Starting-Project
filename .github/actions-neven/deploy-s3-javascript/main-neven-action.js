1d5867e1355288deefe2cf89fd0a62b8c1b19553
Neven Dimač <neven.dimac@gmail.com>
2024-09-26 11:57:30 +0200

    Outputting javascript output info

--- .github/actions-neven/deploy-s3-javascript/main-neven-action.js
+++ .github/actions-neven/deploy-s3-javascript/main-neven-action.js
@@ -10,7 +10,8 @@ function runNeven() {
   const s3Uri = `s3://${bucket}`;
   exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`); // aws cli send commands to a s3 bucket
 
-  core.notice('Hello from my custom Javascript Action!');
+  const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+  core.setOutput('website-url', websiteUrl); // ekvivalent echo "cache='${{ inputs.caching-nev }}'">>$GITHUB_OUTPUT, vratiti će output
 }
 
 runNeven();
